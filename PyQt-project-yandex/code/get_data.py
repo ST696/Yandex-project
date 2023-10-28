a998bf65773a48c1b970f074302f7066d7d50ac5
STban <69729551+ST696@users.noreply.github.com>
2023-10-28 19:02:42 +0400

    Update get_data.py

--- PyQt-project-yandex/code/get_data.py
+++ PyQt-project-yandex/code/get_data.py
@@ -1,37 +1,28 @@
 import requests
-from selenium import webdriver
 from bs4 import BeautifulSoup
 
+
 def get_source_html(link):
-    #("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
+    # ("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
     responce = requests.get(link)
 
 
 def herni():
-
     responce = requests.get("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
     soup = BeautifulSoup(responce.text, features="html.parser")
-    class_find = soup.find_all("div", {"class": "lbb023d8c l40cb8252"})
+    chank_bank = soup.find_all("div", {"class": "lf4cbd87d ld6d46e58 lfd76152f"})
+    with open("data_bank.txt", "w", encoding="utf8") as f:
+        for info_bank in chank_bank:
+            info_bank = list(filter(None, info_bank.text.split('\n')))
+            if len(info_bank) > 8:
+                info_bank = info_bank[:8]
+            f.write(f"{info_bank}\n")
     names_banck = soup.find_all("div", {"class": "text-size-5 text-weight-bold"})
-    for name in names_banck:
-        print(name.text)
-    #find_map = class_find.find("span")[0].text
+
     print("*******************************")
     count = 0
-
-    for bunck in class_find:
-        if len(bunck) != 0:
-            if bunck.find('div').text == "Эффективная ставка":
-                print(f"{count}:{bunck.find('div').text} ::: {bunck.find('span').text}")
-            elif bunck.find('div').text == "Сумма":
-                print(f"{count}:{bunck.find('div').text} ::: {bunck.find('span').text}")
-            elif bunck.find('div').text == "Срок":
-                print(f"{count}:{bunck.find_all('div')[0].text} ::: {bunck.find_all('div')[1].text}")
-        count += 1
-
     print("DONE")
-    with open('resp.txt', "w") as f:
-        f.write(responce.text)
+
 def main():
     herni()
 
