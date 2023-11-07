37d61795442260b4d0c65d4c48ec1f576d4b3a0e
STban <st.necromancer@mail.ru>
2023-11-08 00:32:26 +0400

    хоспади спаси

---
+++ PyQt-project-yandex/code/filling_in_db.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+
+class operationDatabase():
+    def input_new_data_in_using_db():
+        with open("data_bank/data_bank.txt", "r") as file:
+            id = 1
+            for bank in file.readlines():
+                con = sqlite3.connect("database/banking_statistics.sql")
+                cur = con.cursor()
+
+                bank = bank.replace("[", '').replace(']', '').split("'")
+                bank = list(filter(lambda x: x != "," and x != "" and x != ", ", bank))
+                bank_name = bank[0]
+                deposit_name = bank[1]
+                percentages = bank[3]
+                amount = bank[5]
+                term = bank[7]
+                cur.execute("INSERT INTO data_bank(id, bank_name, deposit_name, percentages, amount, term) VALUES(?, ?, ?, ?, ?, ?)", (id, bank_name, deposit_name, percentages, amount, term))
+                id += 1
+        con.commit()
+    
+    def delete_db():
+        con = sqlite3.connect("database/banking_statistics.sql")
+        cur = con.cursor()
+        cur.execute("""DELETE FROM data_bank""")
+        con.commit()
+
+
+def main():
+    oper = operationDatabase
+
+if __name__ == "__main__":
+    main()
--- PyQt-project-yandex/code/get_data.py
+++ PyQt-project-yandex/code/get_data.py
@@ -2,17 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_source_html(link):
-    # ("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
-    responce = requests.get(link)
+def website_request(link,
+                    headers={
+                        "Accept": "	*/*",
+                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"
+                    }):
+    request = requests.get(link)
+    src = request.text
+    with open("index.html", "w") as f:
+        f.write(src)
 
+    return src
 
-def herni():
-    responce = requests.get("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
-    soup = BeautifulSoup(responce.text, features="html.parser")
-    chank_bank = soup.find_all("div", {"class": "lf4cbd87d ld6d46e58 lfd76152f"})
+def parsing_2():
+    headers={
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
+                "referer": "https://www.banki.ru/products/deposits/?source=menubloks_deposits"
+            }
+    with open("url_list", "w") as file:
+        for page in range(1, 2):
+            url = f"https://www.banki.ru/products/deposits/api/group/nizhniy_novgorod/?segmentation_page=deposits_main&amount=0&currency=RUB&period=0&special[]=0&special[]=14&special[]=2&special[]=1&special[]=9&special[]=12&special[]=10&type=All&replenishment=0&partial_withdrawal=0&payment_period_per_month=0&capitalization=0&early_termination_method=0&is_no_additional_expenses=0&hide_growing_rates=0&top_hundred_place=0&is_main_page=1&is_only_bankiru_offer=0&sort=popular&order=desc&page={page}&page_type=MAINPRODUCT_SEARCH"
+            request = requests.get(url=url, headers=headers)
+            file.write(request.text)
+            
+
+def parsing(src):
+    request = src
+    # Парснг !!! Необходимо в soup добаваить все банки !!!
+    soup = BeautifulSoup(request, "lxml")
+
+    chunk_bank = soup.find_all("div", {"class": "lf4cbd87d ld6d46e58 lfd76152f"})
+ 
     with open("data_bank.txt", "w", encoding="utf8") as f:
-        for info_bank in chank_bank:
+        for info_bank in chunk_bank:
             info_bank = list(filter(None, info_bank.text.split('\n')))
             if len(info_bank) > 8:
                 info_bank = info_bank[:8]
@@ -20,11 +42,13 @@ def herni():
     names_banck = soup.find_all("div", {"class": "text-size-5 text-weight-bold"})
 
     print("*******************************")
-    count = 0
     print("DONE")
 
+
 def main():
-    herni()
+    #src = website_request("https://www.banki.ru/products/deposits/?source=menubloks_deposits")
+    #parsing(src)
+    parsing_2()
 
 
 if __name__ == "__main__":
